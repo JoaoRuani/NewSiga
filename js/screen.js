51155fe2e0301d4a803a3d33ae77d3d1772f8576
Joao Ruani <jvitornegri@gmail.com>
2019-01-18 14:01:22 -0200

    first commit

--- js/screen.js
+++ js/screen.js
@@ -1,20 +1,38 @@
 navbar = document.querySelector('header');
-sections = document.querySelectorAll('section');
 width = window.innerWidth;
+tables = document.querySelectorAll('.table');
+
 
 if(width < 768){
     navbar.classList.add('fixed-top');
+    hideTable(tables);
+}
+else{
+    showTable(tables);
 }
 
 window.addEventListener('resize', function(){
     width = this.innerWidth;
     if(width < 768){
         navbar.classList.add('fixed-top');
+        hideTable(tables)
     }
     else{
         navbar.classList.remove('fixed-top');
+        showTable(tables)
     }
 });
+
+function showTable(tables){
+    tables.forEach(function(table){
+        table.classList.add('show');
+    })
+}
+function hideTable(tables){
+    tables.forEach(function(table){
+        table.classList.remove('show');
+    })
+}
  /*-----------------------------------------------*/
  
 
